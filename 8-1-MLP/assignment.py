a41b576baeb824eb10ce3813465c5cb4e7fbc13b
chung isaac <isaac815@naver.com>
2024-08-08 12:53:03 +0900

    tensor

--- 8-1-MLP/assignment.py
+++ 8-1-MLP/assignment.py
@@ -3,51 +3,45 @@ from numpytorch import Tensor, nn
 from numpytorch import reshape
 
 
-"""
-Example model.
-If you want to see how main.py works (before you finish the assignment),
-try running it through this model.
-"""
-class MNISTClassificationModel(nn.Module):
-    def __init__(self) -> None:
-        self.seq = nn.Sequential(
-            nn.Linear(784, 256),
-            nn.ReLU(),
-            nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Linear(256, 10, bias=False)
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = reshape(x, (x.shape[0], -1))
-        logits = self.seq(x)
-        return logits
-
-"""
-Your model!
+import numpytorch as npt
+from numpytorch import Tensor, nn
 
 class Conv2d(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int) -> None:
-        pass
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.weight = Tensor(npt.randn(out_channels, in_channels, kernel_size, kernel_size), requires_grad=True)
+        self.bias = Tensor(npt.zeros(out_channels), requires_grad=True)
 
     def forward(self, x: Tensor) -> Tensor:
-        pass
-
+        return npt.conv2d(x, self.weight, self.bias)
 
 class MaxPool2d(nn.Module):
     def __init__(self, kernel_size: int, stride: int) -> None:
-        pass
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
 
     def forward(self, x: Tensor):
-        pass
-
+        return npt.max_pool2d(x, self.kernel_size, self.stride)
 
 class MNISTClassificationModel(nn.Module):
     def __init__(self) -> None:
-        pass
+        super().__init__()
+        self.conv1 = Conv2d(1, 32, 3)  # (batch_size, 1, 28, 28) -> (batch_size, 32, 26, 26)
+        self.pool = MaxPool2d(2, 2)  # (batch_size, 32, 26, 26) -> (batch_size, 32, 13, 13)
+        self.conv2 = Conv2d(32, 64, 3)  # (batch_size, 32, 13, 13) -> (batch_size, 64, 11, 11)
+        self.fc1 = nn.Linear(64 * 11 * 11, 128)  # (batch_size, 64*11*11) -> (batch_size, 128)
+        self.fc2 = nn.Linear(128, 10)  # (batch_size, 128) -> (batch_size, 10)
 
     def forward(self, x: Tensor) -> Tensor:
-        # Input shape: (batch_size, 1, 28, 28)
-        # Return shape: (batch_size, 10)
-        pass
-"""
+        x = self.conv1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.pool(x)
+        x = reshape(x, (x.shape[0], -1))
+        x = npt.relu(self.fc1(x))
+        logits = self.fc2(x)
+        return logits
